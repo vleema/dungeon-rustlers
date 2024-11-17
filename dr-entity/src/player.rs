87c4eb87fb7bf848149184a4c81dd2834905398f
RaoniSilvestre <raoni.silvestre@hotmail.com>
2024-11-16 22:29:19 -0300

    feat: Impl update for player

--- dr-entity/src/player.rs
+++ dr-entity/src/player.rs
@@ -1,7 +1,8 @@
 use bracket_lib::{
     color::{BLACK, WHITE},
-    prelude::{to_cp437, BTerm, Point},
+    prelude::{to_cp437, BTerm, Point, VirtualKeyCode::*},
 };
+use dr_map::Map;
 
 use crate::Player;
 
@@ -18,5 +19,23 @@ impl Player {
             BLACK,
             to_cp437('@'),
         );
+    }
+
+    pub fn update(&mut self, ctx: &mut BTerm, map: &Map) {
+        if let Some(key) = ctx.key {
+            let delta = match key {
+                Left => Point::new(-1, 0),
+                Right => Point::new(1, 0),
+                Up => Point::new(0, -1),
+                Down => Point::new(0, 1),
+                _ => Point::zero(),
+            };
+
+            let new_position = self.position + delta;
+
+            if map.can_enter_tile(new_position) {
+                self.position = new_position
+            }
+        }
     }
 }
